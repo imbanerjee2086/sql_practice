5978e4ae00f64beccb3b277036433746c38b5df8
Raj Banerjee <34864796+imbanerjee2086@users.noreply.github.com>
2026-04-24 03:16:11 +0530

    Update queries.sql

--- basic-queries/queries.sql
+++ basic-queries/queries.sql
@@ -1,1 +1,15 @@
+-- 1. Select all data
+SELECT * FROM customers;
 
+-- 2. Filter data
+SELECT * FROM customers
+WHERE country = 'India';
+
+-- 3. Order data
+SELECT * FROM customers
+ORDER BY customer_name ASC;
+
+-- 4. Group By
+SELECT country, COUNT(*) AS total_customers
+FROM customers
+GROUP BY country;
